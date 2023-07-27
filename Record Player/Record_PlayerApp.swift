fe13069a10cbc821bfde0f8299ca5c1cc5520859
NazarStf <hworker98@gmail.com>
2023-07-27 21:45:42 +0300

    Initial Commit

---
+++ Record Player/Record_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Record_PlayerApp.swift
+//  Record Player
+//
+//  Created by NazarStf on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Record_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
